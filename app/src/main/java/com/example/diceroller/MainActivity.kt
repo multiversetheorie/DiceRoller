da0314d31d1efd3ef6cc22beaf4499f3d5f707b6
Kovács Ádám <the.adam.kovacs@gmail.com>
2019-08-09 11:37:16 +0200

    Added dice images. Modified RNG to show images based on the random value.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +19,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource= when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+}
 
 
 }
